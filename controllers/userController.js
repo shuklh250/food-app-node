fc436eea2e7469db76f0d700920564a093b53cf9
Himanshu shukla <shuklh250@gmail.com>
2025-02-15 16:05:52 +0530

    update user profile,update Password,and create restorent model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 // Get user info
 
 const userModel = require("../models/userModel");
-
+const bcrypt = require("bcryptjs");
 const getUserController = async (req, res) => {
   try {
     // find user
@@ -32,4 +32,95 @@ const getUserController = async (req, res) => {
   }
 };
 
-module.exports = { getUserController };
+// Upadte user
+
+const updateUserController = async (req, res) => {
+  const id = req.body.id;
+
+
+
+  // console.log(id);
+
+  try {
+    const user = await userModel.findById({ _id: id });
+    //  validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "user not found"
+      });
+    }
+
+    // update
+
+    const { userName, address, phone } = req.body;
+
+    if (userName) user.userName = userName;
+    if (address) user.address = address;
+    if (phone) user.phone = phone;
+    // save user
+
+    await user.save();
+    res.status(200).send({
+      success: true,
+      message: "User Update successfully"
+    });
+  } catch (error) {
+    console.log(error);
+   return res.status(500).send({
+      success: false,
+      message: "Error in update Api"
+    });
+  }
+};
+// update password 
+
+const updatepassword = async(req,res) => {
+
+  try {
+    
+    const {email,newpassword} = req.body
+
+    if(!email || !newpassword){
+    return  res.status(500).send({
+        success: false,
+        message: "All fields are require"
+      });
+
+    }
+
+    const user = await userModel.findOne({email})
+
+if(!user){
+  return res.status(429).send({
+    success: false,
+    message: "This email not exist"
+  });
+
+}
+
+let salt = bcrypt.genSaltSync(10);
+const haspassword = await bcrypt.hash(newpassword, salt);
+
+user.password = haspassword;
+
+await user.save();
+res.status(200).send({
+success:true,
+message :"Passowrd reset successfully"
+
+})
+
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success:false,
+      message:"Error in update password Api"
+    })
+  }
+
+}
+
+
+module.exports = { getUserController, updateUserController , updatepassword};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 
 const express = require('express');
-const { getUserController } = require('../controllers/userController');
+const { getUserController,updateUserController , updatepassword} = require('../controllers/userController');
 const authMIddleware = require('../middlewares/authMIddleware');
 const router = express.Router();
 
 // get user || GET
 
 router.get('/getUser', authMIddleware, getUserController )
+// update user
+router.put('/updateuser',authMIddleware,updateUserController)
+
+// forage password user route 
+router.post('/updatepassword',authMIddleware,updatepassword)
 
 module.exports = router
